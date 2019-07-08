4938228d2ffaa64ec8dc216ad2c22ce1b33b9013
Shadoscythe <52679972+Shadoscythe@users.noreply.github.com>
2019-07-08 17:52:42 -0600

    Add files via upload

---
+++ Code/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float enemySpeed = 10;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    void Start ()
+    {
+        target = Waypoints.points[0];
+    }
+
+
+}
